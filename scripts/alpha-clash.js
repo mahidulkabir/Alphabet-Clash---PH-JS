9527cd388b35d69cf028de26bcfad605de49c2d7
mahidulkabir <mahidulkabir8@gmail.com>
2025-05-27 16:02:48 +0600

    added play now button js

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,4 +1,10 @@
+function continueGame(){
+   const alphabet =  getRandomAlphbet();
+    const currentAlpha = document.getElementById('current-alphabet');
+    currentAlpha.innerText= alphabet;
+}
 function play(){
     hideElementById('home-page');
     showElementById('game-page');
+    continueGame();
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -7,3 +7,11 @@ function showElementById(elementID){
     const element =document.getElementById(elementID);
     element.classList.remove('hidden');
 }
+function getRandomAlphbet(){
+    const aphabetArray= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const alphabetSingle = aphabetArray.split('');
+    const randomNo = Math.random()*25;
+    const index = Math.round(randomNo);
+    const randomAlphabet = alphabetSingle[index];
+    return randomAlphabet;
+}
