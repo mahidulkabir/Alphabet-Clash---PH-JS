daf01c46b63c74469a12c63c58d627e8eaf40daf
mahidulkabir <mahidulkabir8@gmail.com>
2025-05-27 23:57:10 +0600

    added play now button js

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,8 +1,13 @@
 function continueGame(){
    const alphabet =  getRandomAlphbet();
-    const currentAlpha = document.getElementById('current-alphabet');
-    currentAlpha.innerText= alphabet;
+    const currentAlphabet = document.getElementById('current-alphabet');
+    currentAlphabet.innerText= alphabet;
+    setBackgroundColor(alphabet);
 }
+function keyPressedFuntion(){
+    console.log('a key was pressed')
+}
+document.addEventListener('keyup', keyPressedFuntion);
 function play(){
     hideElementById('home-page');
     showElementById('game-page');
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,14 +1,21 @@
+// hide the section 
 function hideElementById(elementID){
 const element = document.getElementById(elementID);
 element.classList.add('hidden');
 }
-
+// show the section 
 function showElementById(elementID){
     const element =document.getElementById(elementID);
     element.classList.remove('hidden');
 }
+// set background color 
+function setBackgroundColor (elementID){
+    const element = document.getElementById(elementID);
+    element.classList.add('bg-orange-400');
+}
+// getting random alphabet 
 function getRandomAlphbet(){
-    const aphabetArray= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const aphabetArray= 'abcdefghijklmnopqrstuvwxyz';
     const alphabetSingle = aphabetArray.split('');
     const randomNo = Math.random()*25;
     const index = Math.round(randomNo);
