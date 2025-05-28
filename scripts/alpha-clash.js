b2a67deec312d634c5a4318060b53cd54119adc1
mahidulkabir <mahidulkabir8@gmail.com>
2025-05-28 15:40:49 +0600

    added button pressed condition

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -4,10 +4,26 @@ function continueGame(){
     currentAlphabet.innerText= alphabet;
     setBackgroundColor(alphabet);
 }
-function keyPressedFuntion(){
-    console.log('a key was pressed')
-}
+function keyPressedFuntion(event){
+    // player pressed 
+    const playerPressed = event.key;
+    
+    // expected key 
+    const currentAlphabet = document.getElementById('current-alphabet');
+    const expectedKey = currentAlphabet.innerText.toLowerCase();
+
+    // conditional statement
+    if (playerPressed === expectedKey){
+        continueGame();
+    }else{
+        console.log('you are wrong');
+    }
+    
+}   
+
 document.addEventListener('keyup', keyPressedFuntion);
+
+
 function play(){
     hideElementById('home-page');
     showElementById('game-page');
